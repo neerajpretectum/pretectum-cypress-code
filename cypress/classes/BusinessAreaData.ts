b14321ab231ace25b3c5d86244ecdc8c68741c8b
MariaPretectum <mariaa@pretectum.com>
2024-12-18 13:48:03 +0500

    Updated BusinessAreaData

--- cypress/classes/BusinessAreaData.ts
+++ cypress/classes/BusinessAreaData.ts
@@ -1,15 +1,9 @@
 import CypressTestIds from  "../classes/CypressTestIDs"
-//import CypressTestIds from  '../../../front-end/src/cypress/CypressTestIds'
-
 import { TestBase } from "./TestBase"
 
 
-//var D_description ='South Asian Countries'
 var purpose ='Picklist For Validation'
-var Data_tag='Date (start Date of business)'
-
-
-
+var Data_tag='language (English)'
 
 export class BADataTest extends TestBase{
 
@@ -20,15 +14,15 @@ openBAData(){
     this.OpenURL();
 
     //login into portal with valid creadentials         
-   
     this.Login();
+    
     //open configuration
     cy.get(this.TestIDLocator(CypressTestIds.HEADER_MENU_CONFIGURATION), {timeout: 8_000})
     .should(this.assertBeVisible)
     .click();
 
     //open BAData section
-    cy.get('[data-testid="vertical-menu-configuration-businessareadata"] > .ant-menu-title-content',{timeout:8000})
+    cy.get(this.TestIDLocator(CypressTestIds. VERTICAL_MENU_BUSINESSAREADATA), {timeout: 8_000})
    .should(this.assertBeVisible)
     .click();
 
@@ -43,25 +37,18 @@ addBAdata(){
    
    .click();
 
-    //select buisness area  
-    cy.get('.ant-select-selection-item', { timeout: 20000 })
-    .click();
-    cy.get('.ant-select-item-option-content', { timeout: 20000 })
-    .contains(this.business_area)
-    .click({force:true});
+    this.selectDropdown(this.TestIDLocator(CypressTestIds.MANAGE_BUSINESS_AREA_DATA_BUSINESS_AREA_NAME_SELECT),this.business_area);
 
     //MANAGE_BUSINESS_AREA_DATA_DOMAIN_NAME_INPUT:
     cy.get(this.TestIDLocator(CypressTestIds. MANAGE_BUSINESS_AREA_DATA_DOMAIN_NAME_INPUT), {timeout: 20_000})
     .should(this.assertBeVisible)
     .type(this.strDomainName)
     .click()
-
-    
+  
     //domain DEscription 
     cy.get(this.TestIDLocator(CypressTestIds. MANAGE_BUSINESS_AREA_DATA_DESCRIPTION_INPUT), {timeout: 20_000})
     .type(this.strDomainName + ' Description', {})
     .click()
-
 
     //Domain Purpose 
     cy.get(this.TestIDLocator(CypressTestIds.MANAGE_BUSINESS_AREA_DATA_PURPOSE_INPUT), {timeout: 20_000})
@@ -69,16 +56,16 @@ addBAdata(){
     .click();
 
     // Select Data Tag
-    cy.get(this.TestIDLocator(CypressTestIds.MANAGE_BUSINESS_AREA_DATA_DATA_TAGS_SELECT), {timeout: 20_000})
-    .click()
-    cy.get('[title= "language (English)"]> .ant-select-item-option-content')
-    .click()
+    this.selectDropdown(this.TestIDLocator(CypressTestIds.MANAGE_BUSINESS_AREA_DATA_DATA_TAGS_SELECT),Data_tag)
+
+  // Close the dropdown with the Escape key
+    cy.get('body').type('{esc}');
 
    cy.get(this.TestIDLocator(CypressTestIds.MANAGE_BUSINESS_AREA_DATA_SAVE_BUTTON), {timeout: 20_000})
    .click()
 
    cy.get(this.TestIDLocator(CypressTestIds.TOAST_ALERT_MESSAGE_SUCCESS), {timeout: 20_000})
-        .should(this.assertBeVisible)
+  .should(this.assertBeVisible)
 
     }
     
@@ -94,14 +81,13 @@ addBAdata(){
          .click()
 
          cy.get(this.TestIDLocator(CypressTestIds.MANAGE_BUSINESS_AREA_DATA_FILE_INPUT), { timeout: 8_000 })
-        .attachFile( fileName, { subjectType: 'drag-n-drop', force: true });
+        .attachFile( fileName, { subjectType: 'drag-n-drop'});
 
         // Wait for the file to upload
          cy.wait(8000);
 
-         cy.get(this.TestIDLocator(CypressTestIds.MANAGE_BUSINESS_AREA_DATA_SAVE_BUTTON), { timeout: 20_000 }) 
-         //.should('contain.text', 'Save and upload') // Check if the correct label is present          
-         .click({force:true});   
+         cy.get(this.TestIDLocator(CypressTestIds.MANAGE_BUSINESS_AREA_DATA_SAVE_BUTTON), { timeout: 20_000 })      
+         .click();   
      
          cy.get(this.TestIDLocator(CypressTestIds.TOAST_ALERT_MESSAGE_SUCCESS), {timeout: 20_000})
         .should(this.assertBeVisible)
@@ -118,15 +104,13 @@ addBAdata(){
        .find(this.TestIDLocator(CypressTestIds.BUSINESS_AREA_DATA_LIST_EDIT_BUTTON), {timeout: 8_000})
        .click()
 
-       cy.get(this.TestIDLocator(CypressTestIds.MANAGE_BUSINESS_AREA_DATA_FILE_INPUT), { timeout: 8_000 })
-      .attachFile( fileName, { subjectType: 'drag-n-drop', force: true });
+       cy.get(this.TestIDLocator(CypressTestIds.MANAGE_BUSINESS_AREA_DATA_FILE_INPUT), { timeout: 20_000 })
+      .attachFile( fileName, { subjectType: 'drag-n-drop' });
 
       // Wait for the file to upload
        cy.wait(8000);
-
        cy.get(this.TestIDLocator(CypressTestIds.MANAGE_BUSINESS_AREA_DATA_SAVE_BUTTON), { timeout: 20_000 }) 
-       //.should('contain.text', 'Save and upload') // Check if the correct label is present          
-       .click({force:true});   
+      .click();   
    
        cy.get(this.TestIDLocator(CypressTestIds.TOAST_ALERT_MESSAGE_SUCCESS), {timeout: 20_000})
       .should(this.assertBeVisible)
@@ -134,44 +118,11 @@ addBAdata(){
   }
 
 
-check_record_value(){
-
-  
-  //cy.wait(5000);
-  cy.get(this.TestIDLocator(CypressTestIds.BUSINESS_AREA_DATA_LIST_TABLE), {timeout: 20_000})
-  .should(this.assertBeVisible)
-  cy.reload({ timeout: 10000 }); // Sets a 10-second timeout
-  cy.contains('td', this.strDomainName) // Locate the cell containing the domain name
-  .parent('tr') // Get the parent row
-  .within(() => {
-    // Locate the "Records" cell in the same row
-    cy.get('td') // Get all cells within this row
-      .eq(6)// Adjust the index to target the "Records" cell
-      .invoke('text') // Get the text content
-      .then((text) => {
-        const recordValue = parseInt(text.trim(), 10); // Parse the text to an integer
-
-        // Check if the value is greater than zero
-        if (recordValue > 0) {
-          cy.log(`The "Records" value for domain ${this.strDomainName} is ${recordValue}, which is greater than zero.`);
-        } else {
-          cy.log(`The "Records" value for domain ${this.strDomainName} is ${recordValue}, which is zero or less.`);
-        }
-
-        // Optional: Add an assertion
-        expect(recordValue).to.be.greaterThan(0, 'Record value should be greater than zero');
-      });
-  })
-
-
-}
-
-
-
 
    // Browse a file
     Browse_CSV_file(fileName: string){
 
+        cy.reload();
         cy.contains(this.strDomainName,{timeout:20_000})
         .parent(this.TD)
         .parent(this.TR)
@@ -180,14 +131,13 @@ check_record_value(){
          .click()
 
          cy.get(this.TestIDLocator(CypressTestIds.MANAGE_BUSINESS_AREA_DATA_FILE_INPUT), {timeout: 20_000})
-         .attachFile(fileName,{force:true});
+         .attachFile(fileName);
         
          // Wait for the file to upload
          cy.wait(10000);
 
          cy.get(this.TestIDLocator(CypressTestIds.MANAGE_BUSINESS_AREA_DATA_SAVE_BUTTON))
-         .should('contain.text', 'Save and upload') // Check if the correct label is present
-         .click({force:true});
+         .click();
 
         cy.get(this.TestIDLocator(CypressTestIds.TOAST_ALERT_MESSAGE_SUCCESS), {timeout: 20_000})
         .should(this.assertBeVisible)
@@ -197,28 +147,10 @@ check_record_value(){
     }
 
 
-    // replace existing data with the new one 
-    replace_data(){
-        cy.contains(this.strDomainName,{timeout:20_000})
-        .parent(this.TD)
-        .parent(this.TR)
-      
-         .find(this.TestIDLocator(CypressTestIds.BUSINESS_AREA_DATA_LIST_EDIT_BUTTON), {timeout: 8_000})
-         .click()
-        
-
-       // Verify that the new data is visible in the preview
-        cy.contains('E02387', {timeout: 20_000})
-        .should(this.assertBeVisible);
-
-         cy.contains('Emily Davis', {timeout: 20_000})
-         .should(this.assertBeVisible);
-
-    }
-
 // appending data
         appending_data(fileName: string){
 
+            cy.reload();
             cy.contains(this.strDomainName,{timeout:20_000})
             .parent(this.TD)
             .parent(this.TR)
@@ -227,7 +159,7 @@ check_record_value(){
              .click()
 
              cy.get(this.TestIDLocator(CypressTestIds.MANAGE_BUSINESS_AREA_DATA_FILE_INPUT), {timeout: 20_000})
-             .attachFile(fileName,{force:true})
+             .attachFile(fileName)
 
               // MANAGE_BUSINESS_AREA_DATA_APPEND_THE_DATA_OPTION
              cy.get(this.TestIDLocator(CypressTestIds.MANAGE_BUSINESS_AREA_DATA_APPEND_DATA_OPTION), {timeout: 20_000})
@@ -235,8 +167,7 @@ check_record_value(){
             // Wait for the file to upload
             cy.wait(8000);
             cy.get(this.TestIDLocator(CypressTestIds.MANAGE_BUSINESS_AREA_DATA_SAVE_BUTTON))
-             .should('contain.text', 'Save and upload') // Check if the correct label is present
-             .click({force:true});
+             .click();
     
             cy.get(this.TestIDLocator(CypressTestIds.TOAST_ALERT_MESSAGE_SUCCESS), {timeout: 20_000})
             .should(this.assertBeVisible)
@@ -252,7 +183,7 @@ check_record_value(){
           .click({force:true})
           cy.wait(8000)
   
-          cy.get('.ant-table-content', {timeout: 8_000})
+          cy.get('.ant-table-content', {timeout: 20_000})
 
           .find('tr') // Find all the rows in the table
           .then((rows) => {
@@ -275,15 +206,13 @@ check_record_value(){
         .parent(this.TD)
         .parent(this.TR)
         .find(this.TestIDLocator(CypressTestIds.BUSINESS_AREA_DATA_LIST_VIEW_BUTTON), {timeout: 20_000})
-        
-        .click({force:true})
+        .click()
     
-        cy.get('.ant-upload-drag-icon', {timeout: 20_000})
+        cy.get(this.TestIDLocator(CypressTestIds.MANAGE_BUSINESS_AREA_DATA_DESCRIPTION_INPUT), {timeout: 20_000})
         .should(this.assertBeVisible)
   
 
     }
-
 
         File_Preview(){
           cy.contains(this.strDomainName, { timeout: 20000 })
@@ -306,7 +235,6 @@ check_record_value(){
         .parent(this.TR)
 
         .find(this.TestIDLocator(CypressTestIds. BUSINESS_AREA_DATA_LIST_VIEW_HISTORY_BUTTON), {timeout: 20_000})
-        //.should(this.assertBeVisible)
         .click()
 
         cy.get('#rc-tabs-0-tab-1',{timeout:8_000})
@@ -315,45 +243,40 @@ check_record_value(){
     }
     
     // Edge_Case_Scenarios
-    Edge_Case_Scenarios(BA:string= '', DN:string = '', Desc:string = '', Purpose: string = ''){
+    Edge_Case_Scenarios(BA:string= '', DN:string = '', Desc:string = '', Purpose: string = '', DT:string=''){
 
         //add buisness area data 
 
         cy.get(this.TestIDLocator(CypressTestIds.BUSINESS_AREA_DATA_ADD_NEW_BUTTON), {timeout: 20_000})
         .click();
-  
-        //select business area  
-        cy.get('.ant-select-selection-item', { timeout: 20000 })
-        .click();
-        cy.get('.ant-select-item-option-content', { timeout: 20000 })
-        .contains(BA)
-        .click({force:true});
-        
-        //Domain Name
+ 
+        this.selectDropdown(this.TestIDLocator(CypressTestIds.MANAGE_BUSINESS_AREA_DATA_BUSINESS_AREA_NAME_SELECT),BA);
 
+        //Domain Name
         cy.get(this.TestIDLocator(CypressTestIds. MANAGE_BUSINESS_AREA_DATA_DOMAIN_NAME_INPUT), {timeout: 20_000})
         .should(this.assertBeVisible)
        .type(DN)
-       .click({force:true})
+       .click()
 
         //Domain Description
         cy.get(this.TestIDLocator(CypressTestIds. MANAGE_BUSINESS_AREA_DATA_DESCRIPTION_INPUT), {timeout: 20_000}) 
+        .focus()
         .type(Desc)
-        .click({force:true})
+        .click()
 
 
         //Domain Purpose 
         cy.get(this.TestIDLocator(CypressTestIds.MANAGE_BUSINESS_AREA_DATA_PURPOSE_INPUT), {timeout: 20_000})
-         cy.get('#basic_domainPurpose',{timeout: 20_000})
         .type(Purpose)
         .click();
 
             // Select Data Tag
-        cy.get(this.TestIDLocator(CypressTestIds.MANAGE_BUSINESS_AREA_DATA_DATA_TAGS_SELECT), {timeout: 20_000})
-        .click()
-        cy.get('[title= "language (English)"]> .ant-select-item-option-content')
-        .click()
+        this.selectDropdown(this.TestIDLocator(CypressTestIds.MANAGE_BUSINESS_AREA_DATA_DATA_TAGS_SELECT),DT)
+    
+        // Close the dropdown with the Escape key
+        cy.get('body').type('{esc}');
 
+        //save
         cy.get(this.TestIDLocator(CypressTestIds.MANAGE_BUSINESS_AREA_DATA_SAVE_BUTTON), {timeout: 20_000})
         .click()
 
@@ -362,32 +285,34 @@ check_record_value(){
        .should(this.assertBeVisible)
 
     }
-    // Edge_Case_Scenarios
-    emptydomain(BA:string= '', DN:string = '', Desc:string = '', Purpose: string = ''){
+
+
+
+
+    // empty domain
+    emptydomain(BA:string= '', DN:string = '', Desc:string = '', Purpose: string = '',DT:string=''){
 
       //add buisness area data 
-
-      cy.get(this.TestIDLocator(CypressTestIds.BUSINESS_AREA_DATA_ADD_NEW_BUTTON), {timeout: 20_000})
+       cy.get(this.TestIDLocator(CypressTestIds.BUSINESS_AREA_DATA_ADD_NEW_BUTTON), {timeout: 20_000})
       .click();
 
-      //select business area  
-      cy.get('.ant-select-selection-item', { timeout: 20000 })
-      .click();
-      cy.get('.ant-select-item-option-content', { timeout: 20000 })
-      .contains(BA)
-      .click({force:true});
-      
+      this.selectDropdown(this.TestIDLocator(CypressTestIds.MANAGE_BUSINESS_AREA_DATA_BUSINESS_AREA_NAME_SELECT),BA);
+
       //Domain Description
       cy.get(this.TestIDLocator(CypressTestIds. MANAGE_BUSINESS_AREA_DATA_DESCRIPTION_INPUT), {timeout: 20_000}) 
       .type(Desc)
-      .click({force:true})
-
+      .click()
 
       //Domain Purpose 
       cy.get(this.TestIDLocator(CypressTestIds.MANAGE_BUSINESS_AREA_DATA_PURPOSE_INPUT), {timeout: 20_000})
-       cy.get('#basic_domainPurpose',{timeout: 20_000})
       .type(Purpose)
       .click();
+
+      //DATA TAG
+      this.selectDropdown(this.TestIDLocator(CypressTestIds.MANAGE_BUSINESS_AREA_DATA_DATA_TAGS_SELECT),DT)
+
+      // Close the dropdown with the Escape key
+      cy.get('body').type('{esc}');
 
       cy.get(this.TestIDLocator(CypressTestIds.MANAGE_BUSINESS_AREA_DATA_SAVE_BUTTON), {timeout: 20_000})
       .click()
@@ -397,21 +322,30 @@ check_record_value(){
 
   }
 
-  empty_desc_and_purpose(BA:string= '', DN:string = '', Desc:string = '', Purpose: string = ''){
+
+  // empty_desc_and_purpose
+  empty_desc_and_purpose(BA:string= '', DN:string = '', Desc:string = '', Purpose: string = '',DT:string=''){
 
   //add buisness area data 
 
   cy.get(this.TestIDLocator(CypressTestIds.BUSINESS_AREA_DATA_ADD_NEW_BUTTON), {timeout: 20_000})
   .click();
-
-  //select business area  
-  cy.get('.ant-select-selection-item', { timeout: 20000 })
-  .click();
-  cy.get('.ant-select-item-option-content', { timeout: 20000 })
-  .contains(BA)
-  .click({force:true});
-
   
+
+  //select business area 
+   this.selectDropdown(this.TestIDLocator(CypressTestIds.MANAGE_BUSINESS_AREA_DATA_BUSINESS_AREA_NAME_SELECT),BA); 
+
+//DOMAIN NAME
+  cy.get(this.TestIDLocator(CypressTestIds. MANAGE_BUSINESS_AREA_DATA_DOMAIN_NAME_INPUT), {timeout: 20_000})
+        .should(this.assertBeVisible)
+       .type(DN)
+       .click()
+
+
+  //DATA TAG
+  this.selectDropdown(this.TestIDLocator(CypressTestIds.MANAGE_BUSINESS_AREA_DATA_DATA_TAGS_SELECT),DT)
+   // Close the dropdown with the Escape key
+   cy.get('body').type('{esc}');
 
   cy.get(this.TestIDLocator(CypressTestIds.MANAGE_BUSINESS_AREA_DATA_SAVE_BUTTON), {timeout: 20_000})
   .click()
