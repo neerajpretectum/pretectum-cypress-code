ea719ead217a1e964a2fc49070f58d784038277f
MariaPretectum <mariaa@pretectum.com>
2024-12-18 13:25:29 +0500

    Updated BusinessAreaData

--- cypress/e2e/BusinessAreaData.cy.ts
+++ cypress/e2e/BusinessAreaData.cy.ts
@@ -116,34 +116,21 @@ it('View Business Area Data HISTORY',()=>{
 
 it('Edge Case Scenario : Empty Domain', function() {
 
-    var businesArea='Test Business Area'
-    var D_description1 ='Description for test case scenarios'
-    var purpose1 ='Picklist For Validation'
-    var Domain_name1 =''
-
-    objBADataTest.emptydomain(businesArea,Domain_name1,  D_description1, purpose1);
+    objBADataTest.emptydomain('Test Business Area','','Description for test case scenarios', 'Picklist For Validation','language (English)');
 });
 
 //Domain input  less than minimum length
 
 it('Edge Case Scenario :Domain input  less than minimum length', function() {
 
-    var businesArea='Test Business Area'
-    var D_description1 ='Description for test case scenarios'
-    var purpose1 ='Picklist For Validation'
-    var Domain_name1 ='A'
 
-    objBADataTest.Edge_Case_Scenarios(businesArea,Domain_name1,  D_description1, purpose1);
+    objBADataTest.Edge_Case_Scenarios('Test Business Area','A', 'Description for test case scenarios', 'Picklist For Validation','language (English)');
 });
 
+//Empty description and purpose
 it('Edge Case Scenario :Empty description and purpose', function() {
 
-    var businesArea='Test Business Area'
-    var D_description1 =''
-    var purpose1 =''
-    var Domain_name1 ='Day and date'
-
-    objBADataTest.empty_desc_and_purpose(businesArea,Domain_name1,  D_description1, purpose1);
+    objBADataTest.empty_desc_and_purpose('Test Business Area','Day and date', '',  '', 'language (English)');
 });
 
 //Input start with Special Character 
@@ -151,12 +138,8 @@ it('Edge Case Scenario :Empty description and purpose', function() {
 
 it('Edge Case Scenario :Input start with Special Character', function() {
 
-    var businesArea='Test Business Area'
-    var D_description1 ='Description for test case scenarios'
-    var purpose1 ='Picklist For Validation'
-    var Domain_name1 ='@countries'
 
-    objBADataTest.Edge_Case_Scenarios(businesArea,Domain_name1,  D_description1, purpose1);
+    objBADataTest.Edge_Case_Scenarios('Test Business Area','@countries', 'Description for test case scenarios', 'Picklist For Validation','language (English)');
 });
 
 
@@ -164,12 +147,7 @@ it('Edge Case Scenario :Input start with Special Character', function() {
 
 it('Edge Case Scenario :maximum length domain name   ', function() {
 
-    var businesArea='Test Business Area'
-    var D_description1 ='Description for test case scenarios'
-    var purpose1 ='Picklist For Validation'
-    var Domain_name1 ='Connecting every person around the world through innovation education and technology for a better future  '
-
-    objBADataTest.Edge_Case_Scenarios(businesArea,Domain_name1,  D_description1, purpose1);
+    objBADataTest.Edge_Case_Scenarios('Test Business Area','Connecting every person around the world through innovation education and technology for a better future  ', 'Description for test case scenarios', 'Picklist For Validation','language (English)');
   
 });
 
@@ -177,24 +155,16 @@ it('Edge Case Scenario :maximum length domain name   ', function() {
 
 it('Edge Case Scenario :maximum length domain description    ', function() {
 
-    var businesArea='Test Business Area'
-    var D_description1 ='Our platform is designed to foster global connections by bridging gaps in education, technology, and innovation. We strive to create a collaborative space where individuals, businesses, and communities can come together to share knowledge, resources, and ideas. By leveraging cutting-edge technology and promoting innovative thinking, we empower people from all walks of life to engage, learn, and grow in a constantly evolving digital landscape. Our mission is to inspire positive change and help shape a more inclusive, connected, and innovative future for everyone'
-    var purpose1 =' pick list for validation'
-    var Domain_name1 ='Day and date   '
 
-    objBADataTest.Edge_Case_Scenarios(businesArea,Domain_name1,  D_description1, purpose1);
+    objBADataTest.Edge_Case_Scenarios('Test Business Area','Day and date', 'Our platform is designed to foster global connections by bridging gaps in education, technology, and innovation. We strive to create a collaborative space where individuals, businesses, and communities can come together to share knowledge, resources, and ideas. By leveraging cutting-edge technology and promoting innovative thinking, we empower people from all walks of life to engage, learn, and grow in a constantly evolving digital landscape. Our mission is to inspire positive change and help shape a more inclusive, connected, and innovative future for everyone', 'Picklist For Validation','language (English)');
     
 });
 
 //maximum length Purpose 
 it('Edge Case Scenario :maximum length Domain Purpose   ', function() {
 
-    var businesArea='Test Business Area'
-    var D_description1 ='picklist for validation'
-    var purpose1 ='The purpose of our platform is to drive positive social and technological change on a global scale. Through the integration of advanced educational tools and innovative technologies, we aim to democratize access to resources that empower individuals and communities. Our mission is to create opportunities for collaboration and problem-solving, fostering a world where knowledge and innovation are accessible to all, paving the way for a more inclusive and sustainable future. We believe in the power of collective action and strive to bring about lasting, impactful change in societies across the globe'
-    var Domain_name1 ='Day and date'
-
-    objBADataTest.Edge_Case_Scenarios(businesArea, Domain_name1,  D_description1, purpose1);
+   
+    objBADataTest.Edge_Case_Scenarios('Test Business Area','Countries', 'Description for test case scenarios', 'The purpose of our platform is to drive positive social and technological change on a global scale. Through the integration of advanced educational tools and innovative technologies, we aim to democratize access to resources that empower individuals and communities. Our mission is to create opportunities for collaboration and problem-solving, fostering a world where knowledge and innovation are accessible to all, paving the way for a more inclusive and sustainable future. We believe in the power of collective action and strive to bring about lasting, impactful change in societies across the globe','language (English)');
    
 });
 
